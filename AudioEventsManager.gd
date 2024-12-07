fec3d2cd281c70f34398f53eedf8d3f41ea99ee1
JaydonXOneGitHub <131696751+JaydonXOneGitHub@users.noreply.github.com>
2024-12-07 18:11:44 -0500

    Add scripts

---
+++ AudioEventsManager.gd
@@ -0,0 +1,76 @@
+class_name AudioEventsManager
+extends Node
+
+@export var audio_player : AudioStreamPlayer
+@export var segments: Array[Segment] = []
+@export var autoplay: bool = false
+
+var _current_segment: Segment
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	if not audio_player:
+		var p: Node = get_parent()
+		if p is AudioStreamPlayer:
+			audio_player = p as AudioStreamPlayer
+	if not audio_player:
+		push_error("Audio player not found!")
+		return
+
+	if segments.is_empty():
+		push_error("No segments have been selected!")
+		return
+
+	_validate_segments()
+
+	if autoplay:
+		play()
+		switch_segments(0)
+
+## Plays the audio stream (and from the start of a segment if one is selected)
+func play(from_position: float = 0.0) -> void:
+	audio_player.play(from_position)
+
+	if _current_segment:
+		audio_player.seek(_current_segment.start)
+
+## Stops the audio
+func stop() -> void:
+	audio_player.stop()
+
+func _validate_segments() -> void:
+	for i in segments:
+		if i.end <= i.start:
+			push_error("Segment end must be greater than start: %s" % i)
+
+## Switches the segments
+func switch_segments(segment_index: int) -> void:
+	if segment_index >= segments.size():
+		push_error("No more segments left!")
+		return;
+		
+	if segment_index < 0:
+		push_error("Index cannot be negative!")
+		return;
+
+	_current_segment = segments[segment_index]
+
+	if _current_segment:
+		audio_player.seek(_current_segment.start)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	if _current_segment and audio_player.playing:
+		if audio_player.get_playback_position() > _current_segment.end:
+			if _current_segment.stops_audio:
+				stop()
+				return
+				
+			if _current_segment.loops:
+				audio_player.seek(_current_segment.start)
+			else:
+				var next_index: int = segments.find(_current_segment) + 1
+				if next_index < segments.size():
+					switch_segments(next_index)
+				else:
+					stop()  # End playback after the last segment
---
+++ Segment.gd
@@ -0,0 +1,11 @@
+class_name Segment
+extends Resource
+
+@export var loops: bool = false
+@export var stops_audio: bool = false
+
+## The start of the audio segment
+@export var start: float
+
+## The end of an audio segment
+@export var end: float
